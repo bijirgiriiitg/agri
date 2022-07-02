e90cae8ab772bc2508abf4034a5883f65e5079a3
Bijirgiri Basumatary <75986148+bijirgiriiitg@users.noreply.github.com>
2022-07-02 21:54:41 +0530

    Update food.js

--- pages/prd6/food.js
+++ pages/prd6/food.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Tabs, Tab, TabPanel } from "../../components/prd6/tabs/tabss";
 import { Button } from "react-bootstrap";
 import App8 from "./mod";
+import { useNavigate } from "react-router-dom";
 import styles from "./styles.module.css";
 import { MdBookmarkBorder, MdLens} from "react-icons/md";
 import Chevron from "react-chevron";
@@ -17,6 +18,7 @@ const TabPanelContainer = styled.div`
 `;
 
 const Appk=()=> {
+  const navigate = useNavigate()
   const [activeTab, setActiveTab] = useState(2);
 
   const handleChange = (e, value) => {
@@ -39,7 +41,7 @@ const Appk=()=> {
         <TabPanel value={activeTab} selectedIndex={2}>
           <h1 className={styles.tab31}>Food Technology : Agriculture machinery</h1>
           <div className={styles.bt01}>
-            <Button className={styles.bt0}><MdBookmarkBorder className={styles.oye100} />Bookmark</Button>
+            <Button onClick={() => navigate('/bookmarks')}   className={styles.bt0}><MdBookmarkBorder className={styles.oye100} />Bookmark</Button>
           </div>
           <img className={styles.org} src="images/111.jpg" alt="/"/>
           <p className={styles.tab4}>What will you learn ?</p>
@@ -52,9 +54,9 @@ const Appk=()=> {
           <p className={styles.tab9}><MdLens className={styles.tab7} />WLorem ipsum dolor sit amet, consectetur ad ipiscing elit.</p>
           <p className={styles.tab10}><MdLens className={styles.tab7} />WLorem ipsum dolor sit amet, consectetur ad ipiscing elit.</p>
           <div className={styles.bt3}>
-            <Button className={styles.bt4}>Mark as Complete</Button>
-            <Button className={styles.bt5}>Join the Discussion</Button>
-         </div>
+            <Button onClick={() => navigate('/notes/1')} className={styles.bt4}>Mark as Complete</Button>
+            <Button onClick={() => navigate('/topics')} className={styles.bt5}>Join the Discussion</Button>
+          </div>
         </TabPanel>
         <App8/>
       </TabPanelContainer>
